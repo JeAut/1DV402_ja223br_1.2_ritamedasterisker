c5b57c119d557598b51f157a2c4d88f78bde1a1d
JeAut <ja223br@student.lnu.se>
2014-09-17 16:51:28 +0200

    Labb 1.2 klar

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row <= 24; row++)
+            {
+             switch (row % 3)
+             {
+                 case 0:
+                     Console.ForegroundColor = ConsoleColor.Yellow;
+                     break;
+
+                 case 1:
+                     Console.ForegroundColor = ConsoleColor.Magenta;
+                     break;
+
+                 case 2:
+                     Console.ForegroundColor = ConsoleColor.Green;
+                     break;
+             }
+                     for (int col = 0; col <= 39; col++)
+                     {
+                         Console.Write("* ");
+                     }
+
+                     Console.WriteLine("");
+
+
+                     
+                     if (row % 2 == 0)
+                     {
+                         Console.Write(" ");
+                     }
+
+                     Console.ResetColor();
+            } 
+
+        }
+    }
+}
